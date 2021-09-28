05a6a9bc102c1ddb298a6f8e7b728742773ce08a
erikadmm <erikamartinezm06@gmail.com>
2021-09-27 23:06:46 -0500

    [ADD]Implement encrypt method

---
+++ AES-test/AES-test/IServiceAES.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AES_test
+{
+    [ServiceContract]
+    public interface IServiceAES
+    {
+        [OperationContract]
+        string GetDecryption(string inputMessage, string iv, string pass);
+    }
+
+}
---
+++ AES-test/AES-test/ServiceAES.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AES_test
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServiceAES : IServiceAES
+    {
+        public string GetDecryption(string inputMessage, string iv, string pass)
+        {
+            try
+            {
+                var AES = new System.Security.Cryptography.RijndaelManaged();
+                var Buffer = Convert.FromBase64String(inputMessage);
+                AES.Key = Convert.FromBase64String(pass);
+                AES.IV = Convert.FromBase64String(iv);
+                AES.Mode = System.Security.Cryptography.CipherMode.CBC;
+                AES.Padding = System.Security.Cryptography.PaddingMode.PKCS7;
+
+                System.Security.Cryptography.ICryptoTransform DESDecrypter = AES.CreateDecryptor();
+
+                return ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length));
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
